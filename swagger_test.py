3c0471d2c3a922c14ff0aa734b56ee7d3e6bbcd4
encima <encima@gmail.com>
2018-02-18 19:58:57 +0100

    added option to delete issues and join issues for models (or endpoints at least)

--- swagger_test.py
+++ swagger_test.py
@@ -10,38 +10,52 @@ class SwaggerTest:
         self.project = self.gl.projects.get(repo)
         if self.project is not None:
             print(self.gl)
-            self.swagger = self.read_swagger(swagger)
-            self.parse_swagger()
+            #self.swagger = self.read_swagger(swagger)
+            #self.parse_swagger()
 
     def read_swagger(self, path):
         with open(path, 'r') as json_file:
             return json.load(json_file)
 
+    def delete_issues(self, term=None):
+        issues = self.project.issues.list(state='opened', per_page=100)
+        found = False
+        for issue in issues:
+            print(issue.title)
+            if term and term in issue.title:
+                found = True
+                issue.state_event = 'close'
+                issue.save()
+        if found:
+            self.delete_issues(term)
+
     def parse_swagger(self):
         for entry in self.swagger['paths']:
+            desc_text = "{}\n".format(entry)
             methods = self.swagger['paths'][entry]
             for key in methods:
                 url = "{}{}".format(self.swagger['basePath'], entry)
-                desc_text = """URL to test: {}. 
+                desc_text += """\nURL to test: {}. 
                 Parameters: {} 
                 Purpose: {} """.format(url, methods[key]['parameters'], methods[key]['summary'])
-                issue = {
-                    'title': '[TO TEST] {} - {}'.format(key.upper(), entry),
-                    'description': desc_text,
-                    'labels': ['testing', 'e2e']
-                    }
-                existing_issues = self.project.issues.list(search=issue['title'])
-                if existing_issues is not None and len(existing_issues) == 0:
-                    print('Creating {}'.format(issue['title']))
-                    self.project.issues.create(issue)
-                else:
-                    print('Issue already exists')
+            issue = {
+                'title': '[TO TEST] {} - {}'.format(key.upper(), entry),
+                'description': desc_text,
+                'labels': ['testing', 'e2e']
+                }
+            existing_issues = self.project.issues.list(search=issue['title'])
+            if existing_issues is not None and len(existing_issues) == 0:
+                print('Creating {}'.format(issue['title']))
+                self.project.issues.create(issue)
+            else:
+                print('Issue already exists')
             print('-----')
 
 
 
 if len(sys.argv) != 2:
     s = SwaggerTest(sys.argv[1], sys.argv[2])
+    s.delete_issues()
 else:
     print('Usage: swagger_test.py <SWAGGER PATH> <GROUP/REPO>')
 
